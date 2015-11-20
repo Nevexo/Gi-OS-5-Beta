a9b57115c6cbd477eb9b1e6c76a749fac4eba4be
gi-os <gio@gi-os.com>
2015-11-19 22:00:11 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,1 @@
- function GetClock(){
-      var d=new Date();
-      var nhour=d.getHours(),nmin=d.getMinutes(),ap;
-      if(nhour==0){ap=" AM";nhour=12;}
-      else if(nhour<12){ap=" AM";}
-      else if(nhour==12){ap=" PM";}
-      else if(nhour>12){ap=" PM";nhour-=12;}
-      
-      if(nmin<=9) nmin="0"+nmin;
-      
-      document.getElementById('clockbox').innerHTML=""+nhour+":"+nmin+ap+"";
-      }
-      
+
